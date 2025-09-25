3431728edba8c556418d144d5079a758167c6a41
Tejas S <tejasvijay96@gmail.com>
2025-09-25 10:07:33 +0530

    Initial

--- SMART_EDUP/settings.py
+++ SMART_EDUP/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'ACCOUNTS',
-    'ASSIGNIMENT',
+    'ASSIGNMENT',
     'PARENTS',
     'STUDENTS',
     'TEACHERS',
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'ACCOUNTS.User'
